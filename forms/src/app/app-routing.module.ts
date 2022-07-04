68c0cc79490e55f9ac881d3fd94f3fc63ccd4443
Matteo Polizeli <polizeli.contato@gmail.com>
2022-07-04 10:32:11 -0300

    bootstrap funcionando
    -- Versão anterior com problemas de bootstrap
    
    Fiz alterações no arquivo angular.json, apliquei outro bootstrap mais atualizado e o botão agora está desabilitado enquanto o formulário não for válido!

--- forms/src/app/app-routing.module.ts
+++ forms/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', pathMatch: 'full', redirectTo: 'templateForm'},
+  { path: 'templateForm', component: TemplateFormComponent },
+  { path: 'dataForm', component: DataFormComponent },
 ];
 
 @NgModule({
